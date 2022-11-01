2d648bf12a943ee4eeb6225a487d7904d7c53ebf
Gregor05 <gregor.obrist@protonmail.com>
2022-11-01 10:34:17 +0100

    Writing the event texts

--- pages/events/sprint-planning.js
+++ pages/events/sprint-planning.js
@@ -5,6 +5,131 @@ import Breadcrumb from '../../util/Breadcrumb'
 export default function SprintPlanning() {
   const pages = [path[0], path[1], path[4], path[7]]
   return (
-    <Breadcrumb pages={pages} />
-  )
+      <div className="flex flex-col justify-center items-center">
+        <Breadcrumb pages={pages} />
+
+        <div className="relative py-16 bg-white overflow-hidden">
+          <div className="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full">
+            <div
+                className="relative h-full text-lg max-w-prose mx-auto"
+                aria-hidden="true"
+            >
+              <svg
+                  className="absolute top-12 left-full transform translate-x-32"
+                  width={404}
+                  height={384}
+                  fill="none"
+                  viewBox="0 0 404 384"
+              >
+                <defs>
+                  <pattern
+                      id="74b3fd99-0a6f-4271-bef2-e80eeafdf357"
+                      x={0}
+                      y={0}
+                      width={20}
+                      height={20}
+                      patternUnits="userSpaceOnUse"
+                  >
+                    <rect
+                        x={0}
+                        y={0}
+                        width={4}
+                        height={4}
+                        className="text-gray-200"
+                        fill="currentColor"
+                    />
+                  </pattern>
+                </defs>
+                <rect
+                    width={404}
+                    height={384}
+                    fill="url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)"
+                />
+              </svg>
+              <svg
+                  className="absolute top-1/2 right-full transform -translate-y-1/2 -translate-x-32"
+                  width={404}
+                  height={384}
+                  fill="none"
+                  viewBox="0 0 404 384"
+              >
+                <defs>
+                  <pattern
+                      id="f210dbf6-a58d-4871-961e-36d5016a0f49"
+                      x={0}
+                      y={0}
+                      width={20}
+                      height={20}
+                      patternUnits="userSpaceOnUse"
+                  >
+                    <rect
+                        x={0}
+                        y={0}
+                        width={4}
+                        height={4}
+                        className="text-gray-200"
+                        fill="currentColor"
+                    />
+                  </pattern>
+                </defs>
+                <rect
+                    width={404}
+                    height={384}
+                    fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)"
+                />
+              </svg>
+              <svg
+                  className="absolute bottom-12 left-full transform translate-x-32"
+                  width={404}
+                  height={384}
+                  fill="none"
+                  viewBox="0 0 404 384"
+              >
+                <defs>
+                  <pattern
+                      id="d3eb07ae-5182-43e6-857d-35c643af9034"
+                      x={0}
+                      y={0}
+                      width={20}
+                      height={20}
+                      patternUnits="userSpaceOnUse"
+                  >
+                    <rect
+                        x={0}
+                        y={0}
+                        width={4}
+                        height={4}
+                        className="text-gray-200"
+                        fill="currentColor"
+                    />
+                  </pattern>
+                </defs>
+                <rect
+                    width={404}
+                    height={384}
+                    fill="url(#d3eb07ae-5182-43e6-857d-35c643af9034)"
+                />
+              </svg>
+            </div>
+          </div>
+          <div className="relative px-4 sm:px-6 lg:px-8">
+            <div className="text-lg max-w-prose mx-auto">
+              <h1>
+              <span className="block text-base text-center text-indigo-600 font-semibold tracking-wide uppercase">
+                EVENTS
+              </span>
+                <span className="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                Daily Scrum Meeting
+              </span>
+              </h1>
+              <p className="mt-8 text-xl text-gray-500 leading-8">
+                Sprint planning is an event in scrum that kicks off the sprint. The purpose of sprint planning is to define what can be delivered in the sprint and how that work will be achieved. Sprint planning is done in collaboration with the whole scrum team.
+
+                In scrum, the sprint is a set period of time where all the work is done. However, before you can leap into action you have to set up the sprint. You need to decide on how long the time box is going to be, the sprint goal, and where you're going to start. The sprint planning session kicks off the sprint by setting the agenda and focus. If done correctly, it also creates an environment where the team is motivated, challenged, and can be successful. Bad sprint plans can derail the team by setting unrealistic expectations.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+  );
 }
