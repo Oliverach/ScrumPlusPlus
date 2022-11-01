5c0e16cb17cabc09d1a93c5f8f188d46b92693a6
Oliverach <zacheo@bbcag.ch>
2022-11-01 10:52:28 +0100

    Product owner info added

--- pages/roles/product-owner.js
+++ pages/roles/product-owner.js
@@ -5,6 +5,145 @@ import Breadcrumb from '../../util/Breadcrumb'
 export default function ProductOwner() {
   const pages = [path[0], path[1], path[3], path[14]]
   return (
-    <Breadcrumb pages={pages} />
+    <div className="flex flex-col justify-center items-center">
+      <Breadcrumb pages={pages} />
+
+      <div className="relative py-16 bg-white overflow-hidden">
+        <div className="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full">
+          <div
+            className="relative h-full text-lg max-w-prose mx-auto"
+            aria-hidden="true"
+          >
+            <svg
+              className="absolute top-12 left-full transform translate-x-32"
+              width={404}
+              height={384}
+              fill="none"
+              viewBox="0 0 404 384"
+            >
+              <defs>
+                <pattern
+                  id="74b3fd99-0a6f-4271-bef2-e80eeafdf357"
+                  x={0}
+                  y={0}
+                  width={20}
+                  height={20}
+                  patternUnits="userSpaceOnUse"
+                >
+                  <rect
+                    x={0}
+                    y={0}
+                    width={4}
+                    height={4}
+                    className="text-gray-200"
+                    fill="currentColor"
+                  />
+                </pattern>
+              </defs>
+              <rect
+                width={404}
+                height={384}
+                fill="url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)"
+              />
+            </svg>
+            <svg
+              className="absolute top-1/2 right-full transform -translate-y-1/2 -translate-x-32"
+              width={404}
+              height={384}
+              fill="none"
+              viewBox="0 0 404 384"
+            >
+              <defs>
+                <pattern
+                  id="f210dbf6-a58d-4871-961e-36d5016a0f49"
+                  x={0}
+                  y={0}
+                  width={20}
+                  height={20}
+                  patternUnits="userSpaceOnUse"
+                >
+                  <rect
+                    x={0}
+                    y={0}
+                    width={4}
+                    height={4}
+                    className="text-gray-200"
+                    fill="currentColor"
+                  />
+                </pattern>
+              </defs>
+              <rect
+                width={404}
+                height={384}
+                fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)"
+              />
+            </svg>
+            <svg
+              className="absolute bottom-12 left-full transform translate-x-32"
+              width={404}
+              height={384}
+              fill="none"
+              viewBox="0 0 404 384"
+            >
+              <defs>
+                <pattern
+                  id="d3eb07ae-5182-43e6-857d-35c643af9034"
+                  x={0}
+                  y={0}
+                  width={20}
+                  height={20}
+                  patternUnits="userSpaceOnUse"
+                >
+                  <rect
+                    x={0}
+                    y={0}
+                    width={4}
+                    height={4}
+                    className="text-gray-200"
+                    fill="currentColor"
+                  />
+                </pattern>
+              </defs>
+              <rect
+                width={404}
+                height={384}
+                fill="url(#d3eb07ae-5182-43e6-857d-35c643af9034)"
+              />
+            </svg>
+          </div>
+        </div>
+        <div className="relative px-4 sm:px-6 lg:px-8">
+          <div className="text-lg max-w-prose mx-auto">
+            <h1>
+              <span className="block text-base text-center text-indigo-600 font-semibold tracking-wide uppercase">
+                ROLES
+              </span>
+              <span className="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                Product Owner
+              </span>
+            </h1>
+            <p className="mt-8 text-xl text-gray-500 leading-8">
+              Product owners are responsible for their product. They focus on analyzing business, customer and market requirements and then prioritize the work ahead for the development team accordingly</p>
+
+            <div className="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
+
+              <h4>
+                The Productowner focuses on:
+              </h4>
+              <ul role="list">
+                <li>Develop and manage the product backlog.</li>
+                <li>Work closely with the business and team to ensure everyone understands the task elements in the product backlog</li>
+                <li>Provide clear direction to the team on which features to ship next</li>
+                <li>Deciding when to ship the product, with a tendency to ship more frequently</li>
+              </ul>
+            </div>
+          </div>
+          <div className="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
+            <p>
+              The product owner is not always the product manager. Product owners focus on ensuring that the development team creates the most value for the business. Also, the product owner should absolutely be a single person. No development team needs different instructions from multiple Product Owners. </p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
